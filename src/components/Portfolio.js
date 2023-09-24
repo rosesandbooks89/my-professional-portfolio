7f9876f93086e1c25c892f6e927dfb1af18ecebc
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-09-24 15:33:29 -0500

    added all project images and links. working on descriptions. need help with css.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,23 +1,16 @@
 import React from "react";
 
-import passwordGenerator from "../assets/Images/password-generator.png";
+import textEditor from "../assets/Images/textEditor.png";
 import weatherDashboard from "../assets/Images/weatherApp.png";
 import myDailyPlanner from "../assets/Images/planner.png";
-// import noteTaker from "../assets/Images/note-taker.png";
-// import gsd from "../assets/Images/gsd.png";
+import noteTaker from "../assets/Images/note-taker.png";
+import gsd from "../assets/Images/gsd.png";
 import yassFlix from "../assets/Images/yassFlix.png";
 
 
 // add 6 projects to the portfolio
 function Portfolio() {
     const projects = [
-        {
-            name: "Password Generator",
-            description: "An application that an employee can use to generate a random password based on criteria they've selected.",
-            image: passwordGenerator,
-            github: "",
-            deployedUrl: "https://rosesandbooks89.github.io/My-Password-Generator/"
-        },
         {
             name: "My Daily Planner",
             description: "An application that allows the user to save events for each hour of the working day.",
@@ -29,26 +22,33 @@ function Portfolio() {
             name: "My Weather App",
             description: "Project 3 description",
             image: weatherDashboard,
-            github: "",
-            deployedUrl: "https://www.google.com"
+            github: "https://github.com/rosesandbooks89/My-Weather-App",
+            deployedUrl: "https://rosesandbooks89.github.io/My-Weather-App/"
         },
-       {
+        {
             name: "Note Taker",
             description: "Project 4 description",
-            image: "",
-            github: "",
-            deployedUrl: "https://www.google.com"
+            image: noteTaker,
+            github: "https://github.com/rosesandbooks89/awesome-note-taker",
+            deployedUrl: "https://awesome-notetaker-acd455f00464.herokuapp.com/"
+        },
+        {
+            name: "Awesome Text Editor",
+            description: " This is a PWA application that allows the user to create, edit, and delete text while offline. When the user comes back online, the text is saved to the database.",
+            image: textEditor,
+            github: "https://github.com/rosesandbooks89/awesome-text-editor",
+            deployedUrl: "https://rosesandbooks89.github.io/My-Password-Generator/"
         },
         {
             name: "GSD",
-            description: "Project 5 description",
-            image: "",
-            github: "",
-            deployedUrl: "https://www.google.com"
+            description: "A task management application that allows the user to create, edit, and delete tasks. This was a group project that I worked on with 3 other developers. This was our second project as students in the UTSA Coding Bootcamp.",
+            image: gsd,
+            github: "https://github.com/rosesandbooks89/GSDone",
+            deployedUrl: "https://gsdone-2f10065746b9.herokuapp.com/"
         },
         {
             name: "Yass Flix",
-            description: "A search engine for movies and where to stream them. This was a group project that I worked on with 3 other developers. This was our first project as students in the UT Coding Bootcamp.",
+            description: "A search engine for movies and where to stream them. This was a group project that I worked on with 3 other developers. This was our first project as students in the UTSA Coding Bootcamp.",
             image: yassFlix,
             github: "https://github.com/bosalinas/ShowBox",
             deployedUrl: "https://bosalinas.github.io/ShowBox/"
@@ -56,7 +56,7 @@ function Portfolio() {
     ];
     
     return (
-        <section className="my-work py-5" id="my-work">
+        <section className="my-work py-5 .bg-secondary.bg-gradient" id="my-work">
             <h2 className="section_title text-center mb-4">My Work</h2>
 
             <div className="row justify-content-center">
