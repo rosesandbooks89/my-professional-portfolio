e198dbf94159058f4c51f0fa98b4bf85d8c93b84
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-09-26 22:27:03 -0500

    fixed footer issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,16 @@ function Footer() {
   return (
     <footer id="contact-me" className="footer">
       <div className="container">
-        <div className="row justify-content-md-between">
-          <div className="col-md-2">
+        <div className="row justify-content-md-center">
+          <div className="col-1 justify-content-md-center">
             <a href="https://github.com/rosesandbooks89">
-              <i className="fab fa-github fa-4x"></i>
+              <i className="fab fa-github fa-2x"></i>
               <p>Github</p>
             </a>
           </div>
-          <div className="col-md-2">
+          <div className="col-1 justify-content-md-center">
             <a href="https://www.linkedin.com/in/emilyarizcorreta/">
-              <i className="fab fa-linkedin fa-4x"></i>
+              <i className="fab fa-linkedin fa-2x"></i>
               <p>LinkedIn</p>
             </a>
           </div>
